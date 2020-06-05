3218086d11bc50d96d6c9a4c32a8fd4a2f658ca1
Sofie Y <Me@Sofies-MacBook-Pro-2.local>
2020-06-05 15:00:00 -0700

    implement delete route for leads

--- controllers/landing.js
+++ controllers/landing.js
@@ -13,12 +13,57 @@ exports.submit_lead = function(req, res, next) {
 	})
 }
 
-exports.show_lead = function(req, res, next) {
+exports.show_leads = function(req, res, next) {
 	// guarantee that the body will be available after executing the findAll even though
 	//call is async
 	models.Lead.findAll().then(leads => {
 		res.render('landing', {title: 'Express', leads: leads});
 
+	})
+}
+
+exports.show_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where : {
+			id : req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead', { lead: lead});
+	})
+}
+
+exports.show_edit_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where : {
+			id : req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead/edit_lead', { lead: lead});
+	})
+}
+
+
+exports.edit_lead = function(req, res, next) {
+	req.params.lead_id
+	req.body.lead_email
+	return models.Lead.update({
+		email:req.body.lead_email
+	}, {
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(result => {
+		res.redirect('/lead/' + req.params.lead_id)
+	})
+}
+
+exports.delete_lead = function(req, res, next) {
+	return models.Lead.destroy({
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(result => {
+		res.redirect('/leads');
 	})
 }
 
--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,9 @@ let landing = require('../controllers/landing');
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
 router.get('/leads', landing.show_leads);
-
+router.get('/lead/:lead_id', landing.show_lead);
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', landing.edit_lead);
+router.post('/lead/:lead_id/delete', landing.delete_lead);
+// get and post have to be the same page
 module.exports = router;
